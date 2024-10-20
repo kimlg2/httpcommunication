5bb008ff60cf68864605e89af23a487468cb5417
gimmin-u <kimlg2015@naver.com>
2024-10-21 05:55:33 +0900

    http라이브러리 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +17,23 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Httpcommunication(),
     );
   }
 }
+
+class Httpcommunication extends StatelessWidget {
+  const Httpcommunication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HTTP 통신'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
+
 
